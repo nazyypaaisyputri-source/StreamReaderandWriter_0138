a3abf6a703227c8cfb9930476a497ea219a1c7b0
nazyypaaisyputri-source <nazyypaaisyputri@gmail.com>
2026-06-02 19:02:09 +0700

    membuat isi dari try untuk nguji eror

--- ManualException.cpp
+++ ManualException.cpp
@@ -1,4 +1,11 @@
 #include <iostream>
 using namespace std;
-
 int main()
+{
+
+    try
+    {
+        cout << "Selamat belajar di prodi TI UMY" << endl;
+        // throw 0.5; //melemparkan sebuah integer maka
+        cout << "Pernyataan tidak akan ditampilkan" << endl;
+    }
